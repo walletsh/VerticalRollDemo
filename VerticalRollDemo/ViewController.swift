06ed815b45a93f1d310324d1845322e3fe49e03e
LeeWorker <lsh249410504@163.com>
2016-12-27 16:12:14 +0800

    Initial Commit

---
+++ VerticalRollDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VerticalRollDemo
+//
+//  Created by imwallet on 16/12/27.
+//  Copyright © 2016年 imWallet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
