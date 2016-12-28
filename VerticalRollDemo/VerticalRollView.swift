89773f3a4e79982c41ed8be1617f12ef5e67a6b1
LeeWorker <lsh249410504@163.com>
2016-12-28 12:41:39 +0800

    first commit

---
+++ VerticalRollDemo/VerticalRollView.swift
@@ -0,0 +1,21 @@
+//
+//  VerticalRollView.swift
+//  VerticalRollDemo
+//
+//  Created by imwallet on 16/12/27.
+//  Copyright © 2016年 imWallet. All rights reserved.
+//
+
+import UIKit
+
+class VerticalRollView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
